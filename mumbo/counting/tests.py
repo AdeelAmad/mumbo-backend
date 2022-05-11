14c19abaa92fa835e184511d69fa5cbbc50f71b7
Adeel <theadeelahmad07@gmail.com>
2022-05-10 21:33:19 -0700

    Added no auth test

--- mumbo/counting/tests.py
+++ mumbo/counting/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Count
+from .views import index
+from django.test.client import RequestFactory
+
+import sys
+sys.path.append('../mumbo')
+
+from management.models import Guild
+
 
 # Create your tests here.
+class CountTestCase(TestCase):
+    def setUp(self):
+        guild = Guild.objects.create(id="12345678901234567890")
+        self.c = Count.objects.create(guild_id=guild)
+        self.factory = RequestFactory()
+
+    def test_get_count_no_auth(self):
+        request = self.factory.get('/counting/')
+        value = index(request)
+        self.assertEqual(value.status_code, 302)
